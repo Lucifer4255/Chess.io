803ea0f56da44cb0b371d4f3dcd0f3b91246149e
Biley <deysuhotra@gmail.com>
2025-02-28 22:49:00 +0530

    Fix env files for frontend 3

--- frontend/app/hooks/useSocket.ts
+++ frontend/app/hooks/useSocket.ts
@@ -3,7 +3,8 @@ import { useEffect, useState } from "react";
 export const useSocket = () => {
     const [socket, setSocket] = useState<WebSocket | null>(null);
     useEffect(() => {
-        const ws = new WebSocket(process.env.WS_URL || "ws://localhost:8000");
+        console.log(import.meta.env.VITE_API_URL);
+        const ws = new WebSocket(import.meta.env.VITE_API_URL||"ws://localhost:8000");
         ws.onopen = () => {
             console.log("Connected to server");
             setSocket(ws);
